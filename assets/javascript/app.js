03dedd559408f7176df9ae4872bc932d2cbf8df3
Landon Evans <evans.landon@gmail.com>
2019-07-20 10:22:37 -0500

    initial html,css,js for layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$('#textarea1').val('New Text');
+M.textareaAutoResize($('#textarea1'));
+import { MDCTextField } from '@material/textfield';
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
