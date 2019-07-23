78ef078c1e20d622f48eb9821528aab74969260b
Brian Hoff <blhoff52002@gmail.com>
2019-07-23 18:45:32 -0500

    initial commit

--- assets/javascript/getMap.js
+++ assets/javascript/getMap.js
@@ -1,9 +1,10 @@
-var arrayOfTrackingPoints = [];
+var totalGas;
 var distance;
 var map;
 var highwayMPG;
 var year;
 var make;
+var convertedDistance;
 
 function initMap() {
     var directionsService = new google.maps.DirectionsService;
@@ -17,115 +18,204 @@ function initMap() {
     });
     directionsDisplay.setMap(map);
 
-    var getMakes = function () {
-        $("#make").slice(1).empty();
-        year = $("#year").val();
-        $.ajax({
-            type: "GET",
-            url: "https://fueleconomy.gov/ws/rest/vehicle/menu/make?year=" + year,
-            dataType: "xml",
-        }).then(function (response) {
-            var jsonResponse = xmlToJson(response).menuItems.menuItem;
-            jsonResponse.forEach(function myFunction(item, index) {
-                var option = $("<option>").addClass("makeOption").text(item.text["#text"]);
-                $("#make").append(option);
-            })
-        })
-    };
-
-    var getModels = function () {
-        $("#model").slice(1).empty();
-
-
-        make = $("#make").val();
-        $.ajax({
-            type: "GET",
-            url: "https://fueleconomy.gov/ws/rest/vehicle/menu/model?year=" + year + "&make=" +
-                make,
-            dataType: "xml",
-        }).then(function (response) {
-            var jsonResponse = xmlToJson(response).menuItems.menuItem;
-            jsonResponse.forEach(function myFunction(item, index) {
-                var option = $("<option>").addClass("modelOption").text(item.text["#text"]);
-                $("#model").append(option);
-            })
-        })
-    };
-
-
-    var getOptions = function () {
-
-        $("#option").slice(1).empty();
-        model = $("#model").val();
-        $.ajax({
-            type: "GET",
-            url: "https://fueleconomy.gov/ws/rest/vehicle/menu/options?year=" + year + "&make=" +
-                make + "&model=" + model,
-            dataType: "xml",
-        }).then(function (response) {
-            var jsonResponse = xmlToJson(response).menuItems.menuItem;
-            if (!Array.isArray(jsonResponse)) {
-                var option = $("<option>");
-                var tdOption = option.text(jsonResponse.text["#text"]);
-                var teOption = tdOption.attr("data-id", jsonResponse.value["#text"])
-                $("#option").append(teOption);
-            } else {
-                jsonResponse.forEach(function myFunction(item, index) {
-                    var option = $("<option>");
-                    var tdOption = option.text(item.text["#text"]);
-                    var teOption = tdOption.attr("data-id", item.value["#text"])
-                    $("#option").append(teOption);
-                })
-            }
-
-        })
-    };
-
-    document.getElementById('year').addEventListener('change', getMakes);
-    document.getElementById('make').addEventListener('change', getModels);
-    document.getElementById('model').addEventListener('change', getOptions);
-
-
     $("#submit").on("click", function myfunction() {
         event.preventDefault();
+        $("#results").empty();
+        $("#autocompleteEnd").attr("style", "");
+        $("#autocompleteStart").attr("style", "");
         // console.log(document.getElementById('start').value);
         calculateAndDisplayRoute(directionsService, directionsDisplay);
     })
 
-    // calculateAndDisplayRoute(directionsService, directionsDisplay)
-
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         directionsService.route({
-            origin: $("#start").val(),
-            destination: $("#end").val(),
+            origin: $("#autocompleteStart").val(),
+            destination: $("#autocompleteEnd").val(),
             travelMode: 'DRIVING'
         }, function (response, status) {
             if (status === 'OK') {
                 directionsDisplay.setDirections(response);
                 distance = response.routes[0].legs[0].distance.value;
-                convertedDistance = Math.floor(distance * 0.00062137);
-                $("#totalMeters").text("Total Distance: " + convertedDistance + " miles");
-                // for (var i = 0; i < response.routes[0].overview_path.length; ++i) {
-                //     arrayOfTrackingPoints.push(new google.maps.LatLng(response.routes[0]
-                //         .overview_path[
-                //             i]
-                //         .lat(),
-                //         response.routes[0].overview_path[i].lng()));
-
-                //     // arrayOfTrackingPoints.push(
-                //     //     JSON.stringify(response.routes[0].overview_path[i].lat()),
-                //     //     JSON.stringify(response.routes[0].overview_path[i].lng()))
-                // }
             } else {
-                window.alert('Directions request failed due to ' + status);
+                $("#autocompleteEnd").val('').css("background-color", "rgba(228, 255, 0, 0.34)");
+                $("#autocompleteStart").val('').css("background-color", "rgba(228, 255, 0, 0.34)");
+
+                function moveback() {
+                    $('html, body').animate({
+                        scrollTop: $("#section2").offset().top
+                    }, 500)
+                };
+                moveback();
+
+                $("#results").empty();
+                console.log("Failure");
+                // window.alert('Directions request failed due to ' + status);
             }
-            fuelCalc(distance)
+
+            function fuelCalc() {
+                function getMPG() {
+
+                    var carID = $("#option option:selected").attr("data-id");
+
+                    $.ajax({
+                        type: "GET",
+                        url: "https://www.fueleconomy.gov/ws/rest/vehicle/" + carID,
+                        dataType: "xml",
+
+                    }).then(function (response) {
+                        // carID = xmlToJson(response).menuItems.menuItem[0].value["#text"]
+                        highwayMPG = xmlToJson(response).vehicle.highway08["#text"];
+                        console.log(highwayMPG)
+                        $.ajax({
+                            type: "GET",
+                            url: "http://api.eia.gov/series/?api_key=4e2512a68239a8a4d7fcccd3cea3b0c1&series_id=TOTAL.RUUCUUS.M",
+                        }).then(function (response) {
+                            // Storing an array of results in the results variable
+                            var results = response.series;
+
+                            // Creating variable to drill down to monthly average price of unleaded gas
+                            var avgPriceUnlRegGas = results[0].data[0][1];
+
+                            console.log(avgPriceUnlRegGas);
+
+                            // Variable to create paragraph with gas price info
+                            var p1 = $("<p>").text("National Monthly Average Price of Unleaded Gas: $" + avgPriceUnlRegGas);
+
+                            // Appending gas price info paragraph to gas-price div
+                            $('.gas-price').append(p1);
+
+                            // Function to calculate total price of gas for trip and divide by user input number of people on tirp
+                            var tripCostDividedByFriends = function () {
+
+                                var totalNumOnTrip = 4;
+
+                                // Conversion from meters to miles
+                                var milesOverMeters = 1 / 1609.34;
+
+                                // Total Trip Distance in Miles
+                                var totalTripDistanceMi = Math.round((distance * milesOverMeters) * 100) / 100;
+
+                                // Total gallons of gas used on the trip
+                                var totalGasUsed = Math.round((totalTripDistanceMi / highwayMPG) * 100) / 100;
+
+                                // Total cost of gas
+                                var totalGasCost = Math.round((totalGasUsed * avgPriceUnlRegGas) * 100) / 100;
+
+                                // Total cost of gas per friend
+                                var costOfGasPerFriend = Math.round((totalGasCost / totalNumOnTrip) * 100) / 100;
+
+                                console.log("Total Trip Distance: " + totalTripDistanceMi + " mi");
+                                console.log("Total Gas Used on Trip: " + totalGasUsed + " gallons");
+                                console.log("Total Cost of Gas on Trip: $" + totalGasCost);
+                                console.log("Cost of Gas per Friend $ " + costOfGasPerFriend);
+
+                                var p2 = $("<p>").text("Total Trip Distance: " + totalTripDistanceMi + " mi");
+                                var p3 = $("<p>").text("Total Gas Used on Trip: " + totalGasUsed + " gallons");
+                                var p4 = $("<p>").text("Total Cost of Gas on Trip: $" + totalGasCost);
+                                var p5 = $("<p>").text("Cost of Gas per Friend: $ " + costOfGasPerFriend);
+
+                                $('#results').append(p2);
+                                $('#results').append(p3);
+                                $('#results').append(p4);
+                                $('#results').append(p5);
+
+                            };
+                            tripCostDividedByFriends();
+                        })
+                    })
+                }
+                getMPG();
+            }
+            fuelCalc();
         });
     }
 
-    function fuelCalc(distance) {
-        var gasCost = 2.5;
-        getMPG();
-    }
+
+
+    var startInput = document.getElementById('autocompleteStart');
+    var autocompleteStart = new google.maps.places.Autocomplete(startInput, {
+        types: []
+    });
+    google.maps.event.addListener(autocompleteStart, 'place_changed', function () {
+        var place = autocompleteStart.getPlace();
+    })
+
+    var endInput = document.getElementById('autocompleteEnd');
+    var autocompleteEnd = new google.maps.places.Autocomplete(endInput, {
+        types: []
+    });
+    google.maps.event.addListener(autocompleteEnd, 'place_changed', function () {
+        var place = autocompleteEnd.getPlace();
+    })
 
 }
+
+var getMakes = function () {
+    $("#make").find('option').not(':first').remove();
+    $("#model").find('option').not(':first').remove();
+    $("#option").find('option').not(':first').remove();
+    year = $("#year").val();
+    $.ajax({
+        type: "GET",
+        url: "https://fueleconomy.gov/ws/rest/vehicle/menu/make?year=" + year,
+        dataType: "xml",
+    }).then(function (response) {
+        var jsonResponse = xmlToJson(response).menuItems.menuItem;
+        jsonResponse.forEach(function myFunction(item, index) {
+            var option = $("<option>").addClass("makeOption").text(item.text["#text"]);
+            $("#make").append(option);
+        })
+    })
+};
+
+var getModels = function () {
+    $("#model").find('option').not(':first').remove();
+    $("#option").find('option').not(':first').remove();
+
+    make = $("#make").val();
+    $.ajax({
+        type: "GET",
+        url: "https://fueleconomy.gov/ws/rest/vehicle/menu/model?year=" + year + "&make=" +
+            make,
+        dataType: "xml",
+    }).then(function (response) {
+        var jsonResponse = xmlToJson(response).menuItems.menuItem;
+        jsonResponse.forEach(function myFunction(item, index) {
+            var option = $("<option>").addClass("modelOption").text(item.text["#text"]);
+            $("#model").append(option);
+        })
+    })
+};
+
+
+var getOptions = function () {
+
+    $("#option").find('option').not(':first').remove();
+    model = $("#model").val();
+    $.ajax({
+        type: "GET",
+        url: "https://fueleconomy.gov/ws/rest/vehicle/menu/options?year=" + year + "&make=" +
+            make + "&model=" + model,
+        dataType: "xml",
+    }).then(function (response) {
+        var jsonResponse = xmlToJson(response).menuItems.menuItem;
+        if (!Array.isArray(jsonResponse)) {
+            var option = $("<option>");
+            var tdOption = option.text(jsonResponse.text["#text"]);
+            var teOption = tdOption.attr("data-id", jsonResponse.value["#text"])
+            $("#option").append(teOption);
+        } else {
+            jsonResponse.forEach(function myFunction(item, index) {
+                var option = $("<option>");
+                var tdOption = option.text(item.text["#text"]);
+                var teOption = tdOption.attr("data-id", item.value["#text"])
+                $("#option").append(teOption);
+            })
+        }
+
+    })
+};
+
+document.getElementById('year').addEventListener('change', getMakes);
+document.getElementById('make').addEventListener('change', getModels);
+document.getElementById('model').addEventListener('change', getOptions);
